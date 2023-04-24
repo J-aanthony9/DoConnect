55913baa1b6844e8bcffd4ba30bd406ab350c49d
testsnits <kishinezlife@gmail.com>
2023-04-24 16:07:12 -0400

    chat controller

--- DoConnect-Backend/src/main/java/cogent/university/com/DoConnectBackend/controller/ChatController.java
+++ DoConnect-Backend/src/main/java/cogent/university/com/DoConnectBackend/controller/ChatController.java
@@ -1,11 +1,29 @@
 package cogent.university.com.DoConnectBackend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cogent.university.com.DoConnectBackend.entity.Chat;
+import cogent.university.com.DoConnectBackend.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ChatController {
+    @Autowired
+    ChatService chatService;
+
+    @PostMapping("/addMsg")
+    public Chat addMsg(@RequestBody Chat chat){
+        return chatService.addMsg(chat);
+    }
+
+    @DeleteMapping("/deleteChatById")
+    public void deleteChatById(@PathVariable("id") int id){
+        chatService.deleteChatById(id);
+    }
+
+    @GetMapping("getallmsg")
+    public String getAllMsg(){
+        return chatService.getAllMsgLeft();
+    }
 }
