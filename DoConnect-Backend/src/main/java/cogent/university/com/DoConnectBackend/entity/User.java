850b55a45fba090260fd44a4349f115bdb58dcd5
testsnits <kishinezlife@gmail.com>
2023-04-24 11:23:06 -0400

    user entity

---
+++ DoConnect-Backend/src/main/java/cogent/university/com/DoConnectBackend/entity/User.java
@@ -0,0 +1,25 @@
+package cogent.university.com.DoConnectBackend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_tbl")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private String userType;
+}
