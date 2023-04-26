cf6f4ad1c61d8adb232f0fd05e31db2ae0e78165
testsnits <kishinezlife@gmail.com>
2023-04-26 10:23:27 -0400

    createQuestionComponent

--- DoConnect-Frontend/src/app/app-routing.module.ts
+++ DoConnect-Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { CreateQuestionComponent } from './components/create-question/create-question.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'login',pathMatch:'full'},
   {path: 'register', component:RegisterComponent},
-  {path: 'login', component:LoginComponent}
+  {path: 'login', component:LoginComponent},
+  {path: 'create_question', component:CreateQuestionComponent}
 ];
 
 @NgModule({
