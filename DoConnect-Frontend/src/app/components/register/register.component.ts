4b9aa312defa2618c3096459bf5238df6182e5bf
testsnits <kishinezlife@gmail.com>
2023-04-27 08:21:48 -0400

    validators for register form

--- DoConnect-Frontend/src/app/components/register/register.component.ts
+++ DoConnect-Frontend/src/app/components/register/register.component.ts
@@ -1,38 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder,AbstractControl } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css','../login/login.component.css']
+  styleUrls: ['./register.component.css', '../login/login.component.css']
 })
-export class RegisterComponent {
-  user: User={
-    name:'',
-    username:'',
-    password:'',
-    email:'',
-    usertype:''
+export class RegisterComponent implements OnInit {
+
+
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    email: new FormControl('')
+  });
+  submitted = false;
+
+  constructor(private userService: UserService, private fb: FormBuilder) {
   }
-
-  constructor(private userService: UserService){}
-
-  signUpUser(){
-    const data = {
-      name:this.user.name,
-      username: this.user.username,
-      password:this.user.password,
-      email: this.user.email,
-      usertype:"admin"
-    };
-    this.userService.createUser(data).subscribe({
-      next: (res) => {
-        console.log(res);
-        //navigate later
-      }
-     
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      username: ['', 
+      [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20)
+      ]
+      ],
+      email:['',
+      [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+      ]],
+      password:['',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)
+      ]
+    ]
     });
+
   }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.registerForm.value, null, 2));
+  }
+
+
+  // signUpUser() {
+  //   const data = {
+  //     name: this.user.name,
+  //     username: this.user.username,
+  //     password: this.user.password,
+  //     email: this.user.email,
+  //     usertype: "admin"
+  //   };
+  //   this.userService.createUser(data).subscribe({
+  //     next: (res) => {
+  //       console.log(res);
+  //       //navigate later
+  //     }
+
+  //   });
+  // }
 
 }
