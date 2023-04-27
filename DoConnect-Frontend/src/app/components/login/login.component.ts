f9867ddfb87982429d0e48342253dfa92e6738e2
gmathon18 <gsm18@pitt.edu>
2023-04-27 12:01:10 -0400

    localBranch-push

--- DoConnect-Frontend/src/app/components/login/login.component.ts
+++ DoConnect-Frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +13,57 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup = new FormGroup({
+
+    username: new FormControl(''),
+    password: new FormControl(''),
+
+  });
+  submitted = false;
+
+  constructor(private userService: UserService, private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+
+      username: ['',
+        [
+          Validators.required,
+
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+
+        ]
+      ]
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    } else {
+
+      const val = this.loginForm.value;
+
+
+      this.authService.login(val)
+        .subscribe(
+          () => {
+            console.log("User is logged in");
+            //this.router.navigateByUrl('/');
+          }
+        );
+
+    }
+    console.log(JSON.stringify(this.loginForm.value, null, 2));
+  }
 }
--- DoConnect-Frontend/src/app/components/register/register.component.ts
+++ DoConnect-Frontend/src/app/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder,AbstractControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormBuilder, AbstractControl } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -19,31 +21,31 @@ export class RegisterComponent implements OnInit {
   });
   submitted = false;
 
-  constructor(private userService: UserService, private fb: FormBuilder) {
+  constructor(private userService: UserService, private fb: FormBuilder, private authService: AuthService, private router: Router) {
   }
   ngOnInit(): void {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
-      username: ['', 
-      [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(20)
-      ]
+      username: ['',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20)
+        ]
       ],
-      email:['',
-      [
-        Validators.required,
-        Validators.email,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
-      ]],
-      password:['',
-      [
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(20)
+      email: ['',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+        ]],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20)
+        ]
       ]
-    ]
     });
 
   }
@@ -57,27 +59,37 @@ export class RegisterComponent implements OnInit {
 
     if (this.registerForm.invalid) {
       return;
+    } else {
+      this.signUpUser()
     }
 
     console.log(JSON.stringify(this.registerForm.value, null, 2));
   }
 
 
-  // signUpUser() {
-  //   const data = {
-  //     name: this.user.name,
-  //     username: this.user.username,
-  //     password: this.user.password,
-  //     email: this.user.email,
-  //     usertype: "admin"
-  //   };
-  //   this.userService.createUser(data).subscribe({
-  //     next: (res) => {
-  //       console.log(res);
-  //       //navigate later
-  //     }
+  signUpUser() {
 
-  //   });
-  // }
+    const val = this.registerForm.value;
 
+    console.log(val);
+
+    // if (val.email && val.password) {
+    //   this.authService.login(val.email, val.password)
+    //     .subscribe(
+    //       () => {
+    //         console.log("User is logged in");
+    //         this.router.navigateByUrl('/');
+    //       }
+    //     );
+
+    this.userService.createUser(val).subscribe({
+      next: (res) => {
+        console.log(res);
+        //navigate later
+      }
+
+    });
+
+
+  }
 }
