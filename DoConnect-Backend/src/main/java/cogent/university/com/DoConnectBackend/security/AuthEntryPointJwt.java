934128c4f3fe2f093cf6cbd94450118cf405aafa
testsnits <kishinezlife@gmail.com>
2023-05-03 12:01:53 -0400

    changes

--- DoConnect-Backend/src/main/java/cogent/university/com/DoConnectBackend/security/AuthEntryPointJwt.java
+++ DoConnect-Backend/src/main/java/cogent/university/com/DoConnectBackend/security/AuthEntryPointJwt.java
@@ -1,5 +1,7 @@
 package cogent.university.com.DoConnectBackend.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 
@@ -13,6 +15,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
@@ -23,6 +27,16 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
         logger.error("Unauthorized error: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
-    }
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("path", request.getServletPath());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);    }
 }
